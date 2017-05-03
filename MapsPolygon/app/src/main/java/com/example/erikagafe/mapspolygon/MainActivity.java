5adee0d9c3316f0a96a177f50f12e2cbb74a4518
erikagafe <erikagafe@gmail.com>
2017-05-03 17:48:24 +0200

    location

---
+++ MapsPolygon/app/src/main/java/com/example/erikagafe/mapspolygon/MainActivity.java
@@ -0,0 +1,12 @@
+package com.example.erikagafe.mapspolygon;
+
+import android.app.Activity;
+
+/**
+ * Created by erikagafe on 4/27/17.
+ */
+
+public class MainActivity extends Activity {
+}
+
+
---
+++ MapsPolygon/app/src/main/java/com/example/erikagafe/mapspolygon/MapsActivity.java
@@ -0,0 +1,124 @@
+package com.example.erikagafe.mapspolygon;
+
+import java.util.List;
+
+import android.location.Address;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.ActivityCompat;
+import android.content.pm.PackageManager;
+import android.Manifest;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
+
+    private GoogleMap mMap;
+    private GoogleApiClient mGoogle;
+    private Location mlocation;
+    double lat, lng;
+
+    private Button button;
+    private EditText textEdit;
+
+    LocationManager locationManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);
+        locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            // Check Permissions Now
+            if (ActivityCompat.shouldShowRequestPermissionRationale(this,
+                    Manifest.permission.ACCESS_FINE_LOCATION)) {
+                // Display UI and wait for user interaction
+            } else {
+                ActivityCompat.requestPermissions(
+                        this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 1);
+            }
+            return;
+        }
+        locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(Location location) {
+                double latitude = location.getLatitude();
+                double longitude = location.getLongitude();
+                LatLng latLng = new LatLng(latitude, longitude);
+                Geocoder geocoder = new Geocoder(getApplicationContext());
+                try {
+                    List<Address> adressList = geocoder.getFromLocation(latitude, longitude, 1);
+                    String str = adressList.get(0).getLocality() + "";
+                    str += adressList.get(0).getCountryName();
+                    mMap.addMarker(new MarkerOptions().position(latLng).title(str));
+                    mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, 10.2f));
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void onStatusChanged(String provider, int status, Bundle extras) {
+
+            }
+
+            @Override
+            public void onProviderEnabled(String provider) {
+
+            }
+
+            @Override
+            public void onProviderDisabled(String provider) {
+
+            }
+        });
+
+
+    }
+
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            return;
+        }
+        mMap.setMyLocationEnabled(true);
+
+            // Add a marker in Sydney and move the camera
+            //LatLng sydney = new LatLng(-34, 151);
+            //mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
+            //mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        }
+}
+
+
+
+
+
+
